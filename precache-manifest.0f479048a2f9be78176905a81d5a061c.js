19766d899f7bfb0beb077cc5761b8578909cfc1f
Erastus Ruiru <erastusruiru@Erastuss-MacBook-Pro.local>
2019-02-05 21:13:44 +0300

    Updates

---
+++ precache-manifest.0f479048a2f9be78176905a81d5a061c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b0b4bc0870d3c948c5a0",
+    "url": "/redux/static/js/runtime~main.b0b4bc08.js"
+  },
+  {
+    "revision": "e73ec966f7510e4e8b03",
+    "url": "/redux/static/js/main.e73ec966.chunk.js"
+  },
+  {
+    "revision": "2ab0e4a0d5c64fb69beb",
+    "url": "/redux/static/js/1.2ab0e4a0.chunk.js"
+  },
+  {
+    "revision": "e73ec966f7510e4e8b03",
+    "url": "/redux/static/css/main.e44ec5dc.chunk.css"
+  },
+  {
+    "revision": "295acb318fdc716aa0dd99ac13ac73bd",
+    "url": "/redux/index.html"
+  }
+];
